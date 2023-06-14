655e1b3a86ad3c73a4d2191f3cb22532a969f18d
Mason Grimshaw <mason@indigigenius.org>
2023-06-13 23:47:05 -0600

    Prep for Day 2

---
+++ week_one/notebooks/2 - Day Two/turtle_challenge_4.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+window.setup(width=800, height=800)
+
+t = turtle.Turtle()
+
+# Challenge 4: Draw the outline of a Starquilt!
+
+# YOUR CODE HERE
+
+
+# DON'T TOUCH THIS
+turtle.mainloop()
